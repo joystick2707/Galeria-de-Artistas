dad61634baeffc77338818ec4be0142995983721
Bryan Strey <137365683+joystick2707@users.noreply.github.com>
2025-05-20 18:42:05 -0300

    Create DetalhesObra.swift

---
+++ DetalhesObra.swift
@@ -0,0 +1,43 @@
+import SwiftUI
+
+struct DetalhesObraView: View {
+    let obra: ObraDeArte
+
+    var body: some View {
+        ScrollView {
+            VStack(alignment: .leading, spacing: 16) {
+                Image(systemName: obra.imagemNome)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(maxWidth: .infinity)
+                    .padding()
+                    .background(Color.gray.opacity(0.1))
+                    .clipShape(RoundedRectangle(cornerRadius: 20))
+                
+                Text(obra.titulo)
+                    .font(.title)
+                    .bold()
+                
+                Text("Artista: \(obra.artista)")
+                    .font(.headline)
+                
+                Text("Ano: \(obra.ano)")
+                    .font(.subheadline)
+                
+                Text("Estilo: \(obra.estilo)")
+                    .font(.subheadline)
+                
+                Divider()
+                
+                Text("Descrição")
+                    .font(.headline)
+                
+                Text(obra.descricao)
+                    .font(.body)
+            }
+            .padding()
+        }
+        .navigationTitle(obra.titulo)
+        //.navigationBarTitleDisplayMode(.inline)
+    }
+}
