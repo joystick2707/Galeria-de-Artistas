40b4b40e0319efd68c6e71642b0005fb3358f0a5
Bryan Strey <137365683+joystick2707@users.noreply.github.com>
2025-05-21 14:45:20 -0300

    Update ObraDeArte.swift

--- ObraDeArte.swift
+++ ObraDeArte.swift
@@ -1,26 +1,19 @@
 import Foundation
 
-/// Modelo de dados que representa uma obra de arte.
 struct ObraDeArte: Identifiable {
     
     /// Identificador único da obra.
     let id: UUID
-    
-    /// Título da obra 
+
     let titulo: String
     
-    /// Nome do artista responsável pela obra.
     let artista: String
     
-    /// Ano de criação 
     let ano: Int
-    
-    /// Estilo ou técnica artística da obra 
     let estilo: String
     
     /// Nome do arquivo da imagem associada à obra no Assets.
     let imagemNome: String
     
-    /// Descrição detalhada da obra de arte.
     let descricao: String
 }
